dde4aa8fad2905aa8a11e0f433096350095ecec7
Tomasz Jaworski <tj.toja@gmail.com>
2021-12-08 14:41:07 +0100

    Added some methods in Item.java file

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,21 @@
 public class Item {
     // represent an item to be carried by the rockets
-    String name;
-    int weight;
+    private String name;
+    private int weight;
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
